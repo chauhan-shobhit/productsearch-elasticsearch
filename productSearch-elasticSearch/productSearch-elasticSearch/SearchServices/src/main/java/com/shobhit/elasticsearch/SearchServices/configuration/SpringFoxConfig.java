bee1ebc93eec148137ed949166d9e5050efed1ca
Shobhit Chauhan <chauhan.shobhit@gmail.com>
2020-11-25 21:27:52 -0600

    more fixes

--- productSearch-elasticSearch/productSearch-elasticSearch/SearchServices/src/main/java/com/shobhit/elasticsearch/SearchServices/configuration/SpringFoxConfig.java
+++ productSearch-elasticSearch/productSearch-elasticSearch/SearchServices/src/main/java/com/shobhit/elasticsearch/SearchServices/configuration/SpringFoxConfig.java
@@ -5,11 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 
 @Configuration
+@EnableSwagger2
 public class SpringFoxConfig {
 
   @Bean
@@ -18,7 +22,13 @@ public class SpringFoxConfig {
         .apis(RequestHandlerSelectors.basePackage("com.shobhit.elasticsearch.SearchServices"))
         // .apis(RequestHandlerSelectors.any()) //for enabling all endpoints
         // including actuators, errors, operational
-        .paths(PathSelectors.any()).build();
+        .paths(PathSelectors.any()).build().apiInfo(apiInfo());
+
+  }
+
+  ApiInfo apiInfo() {
+    return new ApiInfoBuilder().title("Product Search").description("Product Search Services using Elastic Search")
+        .termsOfServiceUrl("").version("1.0.0").build();
   }
 
 }
